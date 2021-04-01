5fdd7aa96c2b815e1741b69b75974dd7124efd3c
HSC-1 <kreaoo13@gmail.com>
2021-04-01 17:46:35 +0900

    수정

--- codingmaster.py
+++ codingmaster.py
@@ -1,105 +1,51 @@
-import sys
-<<<<<<< HEAD
-# n_iter = int(sys.stdin.readline())
-=======
+from __future__ import annotations
+from typing import Any, Type
 
-n_iter = int(sys.stdin.readline())
->>>>>>> 66736403a91f27c7d94af1e5b9b3137c47e9a1bc
-Q = []
-# ptr = 0
-def push(value):
-    Q.append(value)
-    # prt += 1
-
-def empty():
-    if len(Q) == 0:
-<<<<<<< HEAD
-        print('1')
-=======
-        print('-1')
->>>>>>> 66736403a91f27c7d94af1e5b9b3137c47e9a1bc
-    else:
-        print('0')
-
-def pop():
-<<<<<<< HEAD
-    try:        
-        print(Q.pop(0))        
-    except:
-=======
-    if len(Q) != 0:
-        Q.pop(0)
-        print(Q.pop(0))
-        
-    else:
->>>>>>> 66736403a91f27c7d94af1e5b9b3137c47e9a1bc
-        print("-1")
-
-def size():
-    return len(Q)
-
-def front():
-    try: 
-        print(Q[0])
-    except:
-        print('-1')
-
-def back():
-    try:
-        print(Q[-1])
-    except:
-        print('-1')
-
-<<<<<<< HEAD
-# for _ in range(n_iter):
-#     a = sys.stdin.readline().split()
-#     if a[0] == 'push':
-#         push(a[1])
-#     elif a[0] == 'pop':
-#         pop()
-#     elif a[0] == 'empty':
-#         empty()
-#     elif a[0] == 'size':
-#         print(size())
-#     elif a[0] == 'front':
-#         front()
-#     elif a[0] == 'back':
-#         back()
-
-Q = []
-push(14)
-print(Q)
-pop()
-pop()
-empty()
-push(100)
-push(10)
-print(size())
-print(Q)
-=======
-for _ in range(n_iter):
-    a = sys.stdin.readline().split()
-    if a[0] == 'push':
-        push(a[1])
-    elif a[0] == 'pop':
-        pop()
-    elif a[0] == 'empty':
-        empty()
-    elif a[0] == 'size':
-        print(size())
-    elif a[0] == 'front':
-        front()
-    elif a[0] == 'back':
-        back()
+class Node:
+    def __init__(self, key: Any, value: Any, left: Node = None,
+                right: Node = None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
 
 
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
 
-# def is_full(self) -> bool:
-#     return self.ptr >= self.capacity
 
-# def pop(self):
-#     if self.is_empty():
-#         raise FixedStack.Full
-#     self.ptr -= 1
-#     return self.stk[self.ptr]
->>>>>>> 66736403a91f27c7d94af1e5b9b3137c47e9a1bc
+    def search(self, key: Any) -> Any:
+
+        p = self.root
+        while True:
+            if p is None:
+                return None
+            if key == p.key:
+                return p.value
+            elif key < p.key:
+                p = p.left
+            else: 
+                p = p.right
+    
+
+    def add(self, key: Any, value: Any) -> bool:
+        def add_node(node: Node, key: Any, value: Any) -> None:
+            if key == node.key:
+                return False
+            elif key < node.key:
+                if node.left is None:
+                    node.left = Node(key, value, None, None)
+                else:
+                    add_node(node.left, key, value)
+            else:
+                if node.right is None:
+                    node.right = Node(key, value, None, None)
+                else:
+                    add_node(node.right, key, value)
+            return True
+            if self.root is None:
+                self.root = Node(key, value, None, None)
+                return True
+            else:
+                return add_node(self.root, key, value)
